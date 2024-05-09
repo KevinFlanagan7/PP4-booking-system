d32ee8529933c383862143326f638bbc5f81437b
KevinFlanagan <flanagank777@gmail.com>
2024-05-09 11:16:58 +0100

    Add Procfile, set debug to false and add allowed host for deployment.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn golflessons.wsgi
--- golflessons/settings.py
+++ golflessons/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$c*p5%*8y=bdp*gisb-$1z1nwk+p692r2tl56k-e($6!*mn_oh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
