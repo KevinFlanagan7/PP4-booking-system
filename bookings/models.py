5f830901d3032247932d6f53b490d3f05ea3ed60
KevinFlanagan <flanagank777@gmail.com>
2024-05-10 11:28:24 +0100

    Add booking and comment models to admin panel.

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    duration = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE, related_name='comment')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
