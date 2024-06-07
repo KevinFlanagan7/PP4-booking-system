a6e9e323c251830d35d31339f934b4a1bde0e4e3
KevinFlanagan <flanagank777@gmail.com>
2024-06-07 14:01:59 +0100

    Run collect static files command for deployment.

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,15 @@
 console.log("Life, The Universe and Everything!");
+
+document.addEventListener("DOMContentLoaded", function() {
+    const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+    const deleteButtons = document.querySelectorAll(".btn-delete");
+    const deleteForm = document.getElementById("deleteForm");
+
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", function(e) {
+            let bookingId = e.target.getAttribute("booking_id");
+            deleteForm.action = `/bookings/${bookingId}/delete_booking/`;
+            deleteModal.show();
+        });
+    });
+});
